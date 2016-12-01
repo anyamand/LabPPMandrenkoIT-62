6d3ec8d1e07ecb4911c4e6d8018cbaf93979a8d3
Anna Mandrenko <anya_mand@ukr.net>
2016-12-01 22:06:10 +0200

    Style fixes
    Small style fixes

--- laba-pp.js
+++ laba-pp.js
@@ -1,6 +1,5 @@
 'use strict';
 const fs = require('fs');
-<<<<<<< HEAD
 /*const util = require('util');
 const log_file = fs.createWriteStream(__dirname + '/laba_pp.log', {flags : 'w'});
 const log_stdout = process.stdout;
@@ -15,7 +14,7 @@ console.log = function() {
 const shemsya = {};
 
 ((o) => {
-
+  
   let mockData = [
     "Пифагор",
     "Гипократ",
@@ -66,7 +65,7 @@ const shemsya = {};
       });
     }
   }
-  
+
   o.toCharCodeArrays = () => array.map((e) => e.split('').map((c) => c.charCodeAt(0)));
 
   o.toWordArrays = () => array.map((e) => e.split(/[\s\t]+/));
@@ -74,80 +73,9 @@ const shemsya = {};
   o.reinitArray = () => {
     array = mockData.slice(0);
   }
-  
   o.searchSubstring = (substr) => array.slice(0).filter(element => element.indexOf(substr) > -1); 
 
   o.searchContainsInText = (text) => array.slice(0).filter(element => text.indexOf(element) > -1);
-  
-=======
-const shemsya = {};
-
-((o) => {
-	
-	let Data = [
-		"Пифагор",
-		"Гипократ",
-		"Евклид",
-		"Архимед",
-		"Николай Коперник",
-		"Теофаст Парацельс",
-		"Андреас Везалий",
-		"Франсуа Виет",
-		"Галилео Галилей",
-		"Иоганн Кеплер",
-		"Вильям Гарвей",
-		"Рене Декарт",
-		"Пьер Ферма",
-		"Блез Паскаль",
-		"Роберт Бойль",
-		"Христиан Гюгенс",
-		"Антони ван Левенгук",
-		"Исаак Ньютон",
-		"Готфрид Лейбниц",
-		"Карл Линней",
-		"Леонард Эйлер",
-		"Михаил Васильевич Ломоносов",
-		"Аадам Смит",
-		"Шарль Ккулон",
-		"Вильям Гершель"
-	];
-
-	let array = Data.slice(0);
-
-	o.getArray = () => array.slice(0);
-
-	o.filterArray = (criteria) => {
-		if (criteria !== undefined) {			
-			array = array.filter(criteria);
-		} else {
-			array = array.filter(element => element[0] >= 'А' && element[0] <= 'Я');
-		}
-	};
-
-	o.sortArray = (criteria) => {
-		if (criteria !== undefined) {
-			array.sort(criteria);
-		} else {			
-			array.sort((a, b) => {
-				if (a > b) return 1;
-				if (a < b) return -1;
-			});
-		}
-	}
-
-	o.toCharCodeArrays = () => array.map((e) => e.split('').map((c) => c.charCodeAt(0)));
-
-	o.toWordArrays = () => array.map((e) => e.split(/[\s\t]+/));
-
-	o.reinitArray = () => {
-		array = Data.slice(0);
-	}
-	o.searchSubstring = (substr) => array.slice(0).filter(element => element.indexOf(substr) > -1); 
-
-    o.searchContainsInText = (text) => array.slice(0).filter(element => text.indexOf(element) > -1);
-
-
->>>>>>> origin/master
 })(shemsya);
 
 //console.log('shemsya.getArray():\n\n', shemsya.getArray(), '\n\n');
