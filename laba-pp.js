4c909d33250dc2bfc05d6d7a745a172d237980f8
anya <anya_mand@ukr.net>
2016-12-03 18:51:05 +0200

    Update laba-pp.js

--- laba-pp.js
+++ laba-pp.js
@@ -29,28 +29,28 @@ let array = [
 ];
 
 console.log('Отфильтрованый массив:\n\n',
-	array.filter((element) => element[0] >= 'А' && element[0] <= 'Я'));
-        			
+    array.filter((element) => element[0] >= 'А' && element[0] <= 'Я'), '\n\n');
+
 console.log('Отсортированый массив:\n\n', array.sort((a, b) => {
     if (a > b) return 1;
     if (a < b) return -1;
-}));
+}), '\n\n');
 
 console.log('Массивы кодов символов:\n\n',
-	array.map((e) => e.split('').map((c) => c.charCodeAt(0))),'\n\n');
+    array.map((e) => e.split('').map((c) => c.charCodeAt(0))), '\n\n');
 
 console.log('Массивы слов в строках:\n\n',
-	array.map((e) => e.split(/[\s\t]+/)), '\n\n');
+    array.map((e) => e.split(/[\s\t]+/)), '\n\n');
 
 ((o) => {
     o.searchSubstring = (substr) => array.slice(0)
-        .filter((element) => element.indexOf(substr) > -1); 
+        .filter((element) => element.indexOf(substr) > -1);
 
     o.searchContainsInText = (text) => array.slice(0)
         .filter((element) => text.indexOf(element) > -1);
 })(userInterface);
 
-console.log('shemsya.searchSubstring():\n\n',
+console.log('searchSubstring():\n\n',
     userInterface.searchSubstring('Николай'), '\n\n');
 console.log('searchContainsInText(text):\n\n',
     userInterface.searchContainsInText('Адам Смит родился в Шотландии'), '\n\n');
